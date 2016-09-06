cb8e3fbf4495ad72471a56e5e9e6bda731713d46
spankly <spankly@neuronet.com.mx>
2016-09-06 18:04:14 -0500

    Adding properties types and default values

--- App.jsx
+++ App.jsx
@@ -3,9 +3,10 @@ import React from 'react';
 class App extends React.Component {
 
   render() {
+    let txt = this.props.txt;
     return (
       <div>
-        Hello
+        {txt}
       </div>
     )
   }
--- main.js
+++ main.js
@@ -2,4 +2,13 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import App from './App.jsx';
 
-ReactDOM.render(<App />, document.getElementById('app'));
+App.propTypes = {
+  txt: React.PropTypes.string,
+  cat: React.PropTypes.number.isRequired
+};
+
+App.defaultProps = {
+  txt: 'The default value is'
+}
+
+ReactDOM.render(<App  cat={5} />, document.getElementById('app'));
